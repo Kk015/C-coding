c1e48d58cf6143b8219fd9898a6ba78c7a17e738
Kunal Mathur <kunalmathur0803@gmail.com>
2024-06-04 21:35:08 +0530

    3

---
+++ BinarySearch/Peakelement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> a{0, 2, 4, 6, 8, 10, 5, 2};
+    int start = 0;
+    int end = a.size() - 1;
+    int ans = -1;
+    int target = 3;
+    while (start <= end)
+
+    {
+    }
+    cout << ans << endl;
+}
