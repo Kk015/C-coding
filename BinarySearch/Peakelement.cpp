ecfbc6856e305e6403f83a54b4df5830047a1f7e
Kunal Mathur <kunalmathur0803@gmail.com>
2024-06-09 12:22:31 +0530

    1

--- BinarySearch/Peakelement.cpp
+++ BinarySearch/Peakelement.cpp
@@ -8,10 +8,18 @@ int main()
     int start = 0;
     int end = a.size() - 1;
     int ans = -1;
-    int target = 3;
-    while (start <= end)
-
+    while (start < end)
     {
+        int mid = start + ((end - start) / 2);
+        if (a[mid] < a[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            ans = a[mid];
+            end = mid;
+        }
     }
     cout << ans << endl;
 }
